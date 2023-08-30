96135a930e22d275de62af71738f4a950e2adbfd
Ethan-Witter-Student <112420827+EthanWitterStudent@users.noreply.github.com>
2023-08-30 15:50:42 +0000

    1-11 finished, 12-15 remain, allneeds testing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
 
 namespace NoviceChallenges
 {
@@ -12,74 +14,130 @@ namespace NoviceChallenges
         // 1. Return the sum of two numbers.
         public static int Add(int a, int b)
         {
-            // TODO: Implement this method.
-            return 0;
+           int c = a + b;
+            return c;
         }
 
         // 2. Given an integer, return true if it's even, else return false.
         public static bool IsEven(int number)
         {
-            // TODO: Implement this method.
-            return false;
+            int remainder = number % 2;
+            if (remainder > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         // 3. Return the largest of three numbers.
         public static int MaxOfThree(int a, int b, int c)
         {
-            // HINT: You might want to use Math.Max function.
-            // TODO: Implement this method.
-            return 0;
+            int Thebigboy;
+            int Potential_bigboy = Math.Max(a, b);
+            if (c > Potential_bigboy)
+            {
+                Thebigboy = c;
+            }
+            else
+            {
+                Thebigboy = Potential_bigboy;
+            }
+            return Thebigboy;
         }
 
         // 4. Return the length of the given string.
         public static int StringLength(string s)
         {
-            // TODO: Implement this method.
-            return 0;
+            int stringlength = s.Length;
+            return stringlength;
         }
 
         // 5. Return true if the string starts with "Hello", otherwise return false.
         public static bool StartsHello(string s)
         {
             // HINT: Use the string method "StartsWith".
-            // TODO: Implement this method.
-            return false;
+            //Ethan here: I couldn't figure out "StartsWith", so I improvised.
+            //May return later?
+            bool doesStartHello = true; 
+            string hello = "Hello";
+            char[] gigaHello = hello.ToCharArray();
+            char [] searchedWord = s.ToCharArray();
+            for (int i = 0; i < hello.Length; i++)
+            {
+                if (gigaHello[i] != searchedWord[i])
+                {
+                    doesStartHello = false;
+                    break;
+                }
+            }
+            return doesStartHello;
         }
 
         // 6. Reverse a given string.
         public static string ReverseString(string s)
         {
             char[] charArray = s.ToCharArray();
-            // TODO: Reverse the charArray.
-            return new string(charArray);
+            char[] ReversedcharArray = new char[charArray.Length];
+            for(int i = 0; i < charArray.Length; i++)
+            {
+                ReversedcharArray[i] = charArray[charArray.Length-i];
+            }
+
+            return new string(ReversedcharArray);
         }
 
         // 7. Return the factorial of a number.
         public static int Factorial(int n)
         {
-            if (n == 0) return 1;
-            // TODO: Calculate the factorial.
-            return 0;
+            if (n == 0) 
+            {
+                return 1;
+            }
+            int totalFactorial = 1;
+            for (int i = n-1; i>= 1; i--)
+            {
+                totalFactorial = totalFactorial*i;
+            }
+            return totalFactorial;
         }
 
         // 8. Check if a number is a prime number.
         public static bool IsPrime(int number)
         {
-            if (number <= 1) return false;
+            if (number <= 1) 
+            {
+                return false;
+            }
             for (int i = 2; i < number; i++)
             {
-                if (number % i == 0) return false;
+                if (number % i == 0) 
+                {
+                    return false;
+                }
             }
-            // TODO: Return the correct boolean value.
-            return false;
+            
+            return true;
         }
 
-        // 9. Return the nth Fibonacci number.
+        // 9. Return the n-th Fibonacci number.
         public static int Fibonacci(int n)
         {
-            if (n <= 1) return n;
-            // TODO: Calculate the nth Fibonacci number.
-            return 0;
+            if (n <= 1) 
+            {
+                return n;
+            }
+            int[] fibonacci = new int[n];
+            fibonacci [0] = 0;
+            fibonacci [1] = 1;
+            for(int i = 2; i < n; i++)
+            {
+                fibonacci[i] = fibonacci[i-1] * fibonacci[i-2];
+            }
+            
+            return fibonacci[n];
         }
 
         // 10. Given an array of integers, return the largest number.
@@ -88,7 +146,10 @@ namespace NoviceChallenges
             int largest = numbers[0];
             for (int i = 1; i < numbers.Length; i++)
             {
-                // TODO: Find the largest number in the array.
+                if (numbers[i] > largest)
+                {
+                    largest = numbers[i];
+                }
             }
             return largest;
         }
@@ -96,15 +157,28 @@ namespace NoviceChallenges
         // 11. Check if a string is a palindrome (reads the same forward and backward).
         public static bool IsPalindrome(string s)
         {
-            // TODO: Determine if the string is a palindrome.
-            return false;
+            bool isItPalindrome = true;
+            char[] normalRead = s.ToCharArray();
+            string reversedWord = ReverseString(s);
+            char[] reversedRead = reversedWord.ToCharArray();
+            for (int i = 0; i<s.Length; i++)
+            {
+                if(normalRead[i] != reversedRead[i])
+                {
+                    isItPalindrome = false;
+                }
+            }
+            return isItPalindrome;
         }
 
         // 12. Given an array of integers, return the sum of its elements.
         public static int ArraySum(int[] numbers)
         {
             int sum = 0;
-            // TODO: Calculate the sum of the array's elements.
+            foreach (int num in numbers)
+            {
+                
+            }
             return sum;
         }
 
